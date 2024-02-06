d843803317d820591c08a52f1fbcb42c92ece502
Denis <strigodenis@gmail.com>
2024-02-06 16:22:36 +0300

    include scss

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,30 @@ export default {
 
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+header {
+  width: 100%;
+  height: 100px;
+  color: $text-light;
+  background-color: $background-accent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+main {
+  width: 100%;
+}
+
+footer {
+  width: 100%;
+  height: 200px;
+  color: $text-light;
+  background-color: $background-accent;
+}
 </style>
---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="header-container">
+    <img class="logo" src="../assets/images/logo.png" alt="Логотип">
+    <nav>
+      <ul class="nav-list">
+        <li class="nav-item">
+          <a href="#leave-request">Оставить заявку</a>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about">О нас</router-link>
+        </li>
+        <li class="nav-item">
+          <a href="#services">Услуги</a>
+        </li>
+        <li class="nav-item">
+          <a href="#specialists">Наши мастера</a>
+        </li>
+        <li class="nav-item">
+          <a href="#contacts">Контакты</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+  width: 100%;
+  height: 100%;
+  padding: 20px 30px;
+  font-size: 20px;
+  color: $text-light;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    width: 50px;
+    height: 50px;
+  }
+}
+
+nav {
+  width: 100%;
+}
+
+.nav-list {
+  padding-left: 50px;
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.nav-item {
+  cursor: pointer;
+
+  a {
+    color: $text-light;
+    font-weight: bold;
+    text-decoration: none;
+    transition: text-decoration 0.5s;
+    
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
