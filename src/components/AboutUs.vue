4bb9228ded5adeb9b3f0dd37c07ebcb2b66cb3f3
Denis <strigodenis@gmail.com>
2024-02-22 23:53:44 +0300

    feat: created about component

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,6 +1,27 @@
 <template>
-  <section class="about">
+  <section id="about">
+    <div class="about-top">
+      <img class="demo-photo" src="../assets/images/service_demo.jpg" alt="service image">
+      <p class="description">
+        У вас сломался телефон? Не включается компьютер? Появился синий экран смерти? Или нужно просто заменить аккумулятор?
+        Тогда вы попали по адресу, сучк... Ладно, что то я заговорился.
+        Батя и его верный (через 3 п#зды колено) родственник всегда готовы вам помочь в решении ваших проблем.
+        Естественно не за бесплатно, но за очень адекватную и, я бы даже сказал символическую цену.
+        Мы всегда укладываемся в срок и делаем услуги, качество которых можно сравнить с автоматом Калашникова.
+        На выходе вы даже не поймете, что у вас была проблема.
+      </p>
+    </div>
 
+    <div class="about-bottom">
+      <p class="description">
+        Мы предоставляем различного рода услуги, чиним как компьтеры, моноблоки, так и мобильные устройства.
+        Делаем много чего, как устранение поломки, неисправности, так и замена каких либо нерабочих или потерявших качество частей.
+        Диагностика бесплатно. Если качество оказанной услуги вас не устроит, то вернем деньги.
+        <span>Оставляйте заявку</span> или сразу приходите в один из филиалов, расскажете и своей проблеме.
+        Мы сделаем все в максимально сжатые сроки.
+      </p>
+      <img class="demo-photo" src="../assets/images/rempkbabyshk1.jpg" alt="service image">
+    </div>
   </section>
 </template>
 
@@ -9,4 +30,31 @@
 
 
 <style lang="scss" scoped>
+#about {
+  background-color: $background-light;
+  color: $text-dark;
+}
+
+.about-top,
+.about-bottom {
+  padding: 50px 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 100px;
+}
+
+.demo-photo {
+  height: 350px;
+}
+
+.description {
+  font-size: 18px;
+  line-height: 36px;
+
+  span {
+    color: $text-accent;
+  }
+}
 </style>
