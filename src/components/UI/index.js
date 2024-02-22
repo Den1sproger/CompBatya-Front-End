a6d944b91320423315d0229d6c276b41764fee1e
Denis <strigodenis@gmail.com>
2024-02-22 22:04:42 +0300

    feat: update UI index

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,6 +1,10 @@
 import FormInput from '@/components/UI/FormInput';
+import LoadingMark from '@/components/UI/LoadingMark';
+import RefreshButton from '@/components/UI/RefreshButton';
 
 
 export default [
-  FormInput
+  FormInput,
+  LoadingMark,
+  RefreshButton,
 ]
